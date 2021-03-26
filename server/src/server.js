caf2442805214f8fe3a1328f32c28b373c74b2fe
simonriedmann <simon.riedmann@web.de>
2021-03-26 12:59:38 +0100

    fixed server issues

--- server/src/server.js
+++ server/src/server.js
@@ -11,8 +11,7 @@ server.use(cors());
 server.use(express.json());
 const DB_NAME = process.env.DB_NAME || 'glutenfree-restaurants';
 
-const connectionString = 'mongodb+srv://garfiled:<password>@cluster0.qdwtp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-/*`mongodb://localhost:27017/${DB_NAME}`;*/
+const connectionString = `mongodb://localhost:27017/${DB_NAME}`;
 
 
 mongoose.connect(connectionString, {
